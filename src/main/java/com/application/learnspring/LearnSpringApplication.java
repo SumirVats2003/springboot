932eac60df1fbb63e251304e141cce8e4009615e
Sumir Vats <sumirvats@gmail.com>
2024-04-06 13:57:54 +0530

    Step 3 - Iteration 1 - Tightly Coupled Game runner and Mario Game

--- src/main/java/com/application/learnspring/LearnSpringApplication.java
+++ src/main/java/com/application/learnspring/LearnSpringApplication.java
@@ -1,13 +1,17 @@
 package com.application.learnspring;
 
-import org.springframework.boot.SpringApplication;
+import com.application.learnspring.game.GameRunner;
+import com.application.learnspring.game.MarioGame;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringApplication.class, args);
+		// SpringApplication.run(LearnSpringApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
 	}
 
 }
---
+++ src/main/java/com/application/learnspring/game/MarioGame.java
@@ -0,0 +1,16 @@
+package com.application.learnspring.game;
+
+public class MarioGame {
+    public void up() {
+        System.out.println("Up");
+    }
+    public void down() {
+        System.out.println("Down");
+    }
+    public void left() {
+        System.out.println("Left");
+    }
+    public void right() {
+        System.out.println("Right");
+    }
+}
